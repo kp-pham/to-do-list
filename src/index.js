3758ce543a0b9312192cbc809e9266b753cd89d4
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-30 20:19:15 -0700

    feat: toggle between light and dark theme
    Create CSS properties for colors of the respective theme.

--- src/index.js
+++ src/index.js
@@ -29,3 +29,11 @@ project.todoItems.forEach(todoItem => {
 
     content.appendChild(task);
 });
+
+function changeTheme() {
+    const root = document.documentElement;
+    const theme = root.className === "dark" ? "light" : "dark";
+    root.className = theme;
+}
+
+document.getElementById("theme-toggle").addEventListener("click", changeTheme);
