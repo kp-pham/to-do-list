eea45d130cd6108ee9bdf7b8c07907240caf8588
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-31 11:36:05 -0700

    feat: create modal for adding tasks
    Provide title, description, due date, and priority level of task.

--- src/add-todo-item.js
+++ src/add-todo-item.js
@@ -1,8 +1,10 @@
 const button = document.getElementById("add-task");
 const modal = document.getElementById("new-task");
 const form = modal.firstChild;
+const cancelButton = document.querySelector(".cancel-form");
 
 const displayModal = () => modal.showModal();
-const closeModal = () => modal.closeModal();
+const closeModal = () => modal.close();
 
 button.addEventListener("click", displayModal);
+cancelButton.addEventListener("click", closeModal);
