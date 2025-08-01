acd2b7e8647d3b23b7d254636630d9d4d0de0255
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-08-01 16:32:58 -0700

    feat: create function to test whether local storage is available
    Use local storage API to implement persistence.

---
+++ src/storage.js
@@ -0,0 +1,16 @@
+function storageAvailable(type) {
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+
+        return true;
+    }
+    catch(error) {
+        return error instanceof DOMException && error.name === "QuotaExceededError" 
+            && storage && storage.length !== 0;
+    }
+}
+
+console.log(storageAvailable("localStorage"));
